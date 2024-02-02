253df12fd94643ddd34d597aad67f9c64b305d81
Sanik10 <109314889+Sanik10@users.noreply.github.com>
2024-02-02 13:43:07 +0300

    Обновление скрипта WeatherForecast

---
+++ OnlineCoursesOrganizationPlatform/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineCoursesOrganizationPlatform.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static List<string> Summaries = new()
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public List<string> Get()
+    {
+        return Summaries;
+    }
+
+    [HttpPost]
+    public IActionResult Add(string name)
+    {
+        Summaries.Add(name);
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Update(int index, string name)
+    {
+        if(index < 0 || index >= Summaries.Count)
+        {
+            return BadRequest("Введен неверный индекс");
+        }
+        
+        Summaries[index] = name;
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int index)
+    {
+        Summaries.RemoveAt(index);
+        return Ok();
+    }
+
+    //[HttpGet(Name = "GetWeatherForecast")]
+    //public IEnumerable<WeatherForecast> Get()
+    //{
+    //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //    {
+    //        Date = DateTime.Now.AddDays(index),
+    //        TemperatureC = Random.Shared.Next(-20, 55),
+    //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //    })
+    //    .ToArray();
+    //}
+}
+
